aa3907b0d5beb244323a7e6fc4e765e5ee99e1ff
XuXo <XuXo@Tianxings-MacBook-Air.local>
2014-01-20 00:08:12 -0500

    Initial Commit

---
+++ SimpleGroups/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SimpleGroups
+//
+//  Created by XuXo on 1/20/14.
+//  Copyright (c) 2014 XuXo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
